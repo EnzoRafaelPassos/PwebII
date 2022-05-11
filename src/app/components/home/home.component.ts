6c1e15917c7e8bd16852cabac7ed1f4622488ecb
EnzoRafaelPassos <enzorafel14@gmail.com>
2022-05-10 22:35:26 -0300

    FrontEnzo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,8 +12,17 @@ export class HomeComponent implements OnInit {
   idProduto: number = 123;
   precoProduto: number = 2.59;
   promocao: boolean = true;
+  foto: string = 'assets/img/crud.png'
 
-  constructor() { }
+  constructor() {
+      console.log('Nome do Produto: ',this.nomeProduto);
+      console.log('Nome do Produto: ',this.anuncio);
+      console.log('Nome do Produto: ',this.idProduto);
+      console.log('Nome do Produto: ',this.precoProduto);
+      console.log('Nome do Produto: ',this.promocao);
+
+
+    }
 
   ngOnInit(): void {
   }
